79b98c3abdb316b22b48b6c0f56073a6b09cca55
lance-jen <lance.jen0706@gmail.com>
2022-12-21 09:13:47 +0000

    comment

--- simpleservice/math/math_test.go
+++ simpleservice/math/math_test.go
@@ -12,10 +12,10 @@ func TestAdd(t *testing.T) {
         t.Error("math.Add FAIL")
     }
 }
-func TestAdd2(t *testing.T) {
-    if Add(1, 2) == 2 {
-        t.Log("math.Add PASS")
-    } else {
-        t.Error("math.Add FAIL")
-    }
-}
+// func TestAdd2(t *testing.T) {
+//     if Add(1, 2) == 2 {
+//         t.Log("math.Add PASS")
+//     } else {
+//         t.Error("math.Add FAIL")
+//     }
+// }
