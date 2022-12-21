d3630929b204bef91b01987f9fb80e0acf5d218d
lance-jen <lance.jen0706@gmail.com>
2022-12-21 08:58:38 +0000

    add func

---
+++ simpleservice/math/math_test.go
@@ -0,0 +1,21 @@
+package math
+
+import "testing"
+
+func TestSomething(t *testing.T) {
+    t.Skip()
+}
+func TestAdd(t *testing.T) {
+    if Add(1, 2) == 3 {
+        t.Log("math.Add PASS")
+    } else {
+        t.Error("math.Add FAIL")
+    }
+}
+func TestAdd2(t *testing.T) {
+    if Add(1, 2) == 2 {
+        t.Log("math.Add PASS")
+    } else {
+        t.Error("math.Add FAIL")
+    }
+}
